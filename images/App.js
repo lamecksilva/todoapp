3421a894c9eb22bd55b46f4c331da4ad070eb9b4
lameck <lameck2@hotmail.com>
2018-11-20 15:25:42 -0300

    Salvando

--- images/App.js
+++ images/App.js
@@ -6,25 +6,28 @@ import {
   View,
   PixelRatio,
   TouchableOpacity,
-  Image
+  Image,
+  Alert
 } from "react-native";
 
 import ImagePicker from "react-native-image-picker";
+import RNFetchBlob from "rn-fetch-blob";
 
 export default class App extends React.Component {
   state = {
     avatarSource: null,
-    videoSource: null,
-    avatartxt: null
+    data: null
   };
 
   selectPhotoTapped() {
     const options = {
-      title: "Select Avatar",
+      title: "Select Image",
       storageOptions: {
         skipBackup: true,
         path: "images"
-      }
+      },
+      maxWidth: 1920,
+      maxHeight: 1080
     };
 
     ImagePicker.showImagePicker(options, response => {
@@ -34,42 +37,56 @@ export default class App extends React.Component {
         console.log("User cancelled image picker");
       } else if (response.error) {
         console.log("ImagePicker Error: ", response.error);
-      } else if (response.customButton) {
-        console.log("User tapped custom button: ", response.customButton);
       } else {
-        const source = { uri: "data:image/jpeg;base64," + response.data };
+        let source = { uri: "data:image/jpeg;base64," + response.data };
 
         this.setState({
-          avatarSource: source
+          avatarSource: source,
+          data: response.data
         });
       }
     });
   }
 
+  uploadPhoto = () => {
+    RNFetchBlob.fetch(
+      "POST",
+      "http://192.168.15.16:8000/api/upload",
+      { "Content-Type": "multipart/form-data" },
+      [
+        {
+          name: "img_documentos",
+          filename: "fotografia.jpeg",
+          data: this.state.data
+        },
+        { name: "tipo_documento", data: "1" },
+        { name: "cliente_id", data: "4" }
+      ]
+    )
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <View
+          style={[styles.avatar, styles.avatarContainer, { marginBottom: 20 }]}
+        >
+          {this.state.avatarSource === null ? (
+            <Text>Select a Photo</Text>
+          ) : (
+            <Image style={styles.avatar} source={this.state.avatarSource} />
+          )}
+        </View>
+
         <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
-          <View
-            style={[
-              styles.avatar,
-              styles.avatarContainer,
-              { marginBottom: 20 }
-            ]}
-          >
-            {this.state.avatarSource === null ? (
-              <Text>Select a Photo</Text>
-            ) : (
-              <Image style={styles.avatar} source={this.state.avatarSource} />
-            )}
-          </View>
+          <Text style={styles.btn}>Select Photo</Text>
         </TouchableOpacity>
 
-        {this.state.avatartxt === null ? (
-          <Text>haha nada aqui </Text>
-        ) : (
-          <Text>{this.state.avatartxt}</Text>
-        )}
+        <TouchableOpacity onPress={this.uploadPhoto.bind(this)}>
+          <Text style={styles.btn}>Upload Photo</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -90,7 +107,10 @@ const styles = StyleSheet.create({
   },
   avatar: {
     borderRadius: 75,
-    width: 150,
-    height: 150
+    width: 350,
+    height: 350
+  },
+  btn: {
+    fontSize: 80
   }
 });
--- reduxImg/android/app/src/main/java/com/reduximg/MainApplication.java
+++ reduxImg/android/app/src/main/java/com/reduximg/MainApplication.java
@@ -1,10 +1,8 @@
-package com.images;
+package com.reduximg;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFetchBlobPackage(),
-            new ImagePickerPackage()
+          new MainReactPackage()
       );
     }
 
