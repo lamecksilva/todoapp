2198a69fcd63d716a756a1089a26fa55e1c257a2
lameck <lameck2@hotmail.com>
2018-12-27 22:34:07 -0300

    Adicionando gambiarras com drawer, stack e switch navigator e.... funciona rsrsr

--- combineNavigators/App.js
+++ combineNavigators/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { Icon } from "react-native-elements";
+import { View, StyleSheet, BackHandler } from "react-native";
+import {
+  Header,
+  Container,
+  Content,
+  Body,
+  Left,
+  Button,
+  Text,
+  Icon
+} from "native-base";
 /**
  * - AppSwitchNavigator
  *    - WelcomeScreen
@@ -18,9 +27,8 @@ import { Icon } from "react-native-elements";
 import {
   createSwitchNavigator,
   createAppContainer,
-  createDrawerNavigator,
-  createBottomTabNavigator,
-  createStackNavigator
+  createStackNavigator,
+  createDrawerNavigator
 } from "react-navigation";
 
 class App extends Component {
@@ -34,99 +42,212 @@ class WelcomeScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button
-          title="Login"
-          onPress={() => this.props.navigation.navigate("Dashboard")}
-        />
-        <Button title="Sign Up" onPress={() => alert("button pressed")} />
+        <Button onPress={() => this.props.navigation.navigate("Dashboard")}>
+          <Text>Login</Text>
+        </Button>
+        <Button onPress={() => alert("button pressed")}>
+          <Text>Sign UP</Text>
+        </Button>
+        <Button onPress={() => this.props.navigation.navigate("CadastroPage")}>
+          <Text>CadastroPage</Text>
+        </Button>
       </View>
     );
   }
 }
 
-class DashboardScreen extends Component {
+class CadastroPage extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackPress);
+  }
+
+  handleBackPress = () => {
+    this.props.navigation.navigate("Welcome"); // works best when the goBack is async
+    return true;
+  };
+
   render() {
+    console.log(this.props);
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>DashboardScreen</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate("Welcome")}
+            >
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Text>{this.props.navigation.state.routeName}</Text>
+          </Body>
+        </Header>
+
+        <Content>
+          <Text style={{ textAlign: "center" }}>
+            {this.props.navigation.state.routeName} aqui trosha
+          </Text>
+        </Content>
+      </Container>
     );
   }
 }
 
 class Feed extends Component {
+  static navigationOptions = {
+    header: null
+  };
   render() {
+    console.log(this.props);
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Feed</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.openDrawer()}
+            >
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Text>{this.props.navigation.state.routeName}</Text>
+          </Body>
+        </Header>
+
+        <Content>
+          <Text style={{ textAlign: "center" }}>
+            {this.props.navigation.state.routeName} aqui trosha
+          </Text>
+        </Content>
+      </Container>
     );
   }
 }
 
 class Settings extends Component {
+  static navigationOptions = {
+    header: null
+  };
   render() {
+    console.log(this.props);
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Settings</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.openDrawer()}
+            >
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Text>{this.props.navigation.state.routeName}</Text>
+          </Body>
+        </Header>
+
+        <Content>
+          <Text style={{ textAlign: "center" }}>
+            {this.props.navigation.state.routeName} aqui trosha
+          </Text>
+        </Content>
+      </Container>
     );
   }
 }
 
 class Profile extends Component {
+  static navigationOptions = {
+    header: null
+  };
   render() {
+    console.log(this.props);
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Profile</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.openDrawer()}
+            >
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Text>{this.props.navigation.state.routeName}</Text>
+          </Body>
+        </Header>
+
+        <Content>
+          <Text style={{ textAlign: "center" }}>
+            {this.props.navigation.state.routeName} aqui trosha
+          </Text>
+        </Content>
+      </Container>
     );
   }
 }
 
-const DashboardTabNavigator = createBottomTabNavigator(
-  {
-    Feed,
-    Profile,
-    Settings
-  },
-  {
-    navigationOptions: ({ navigation }) => {
-      const { routeName } = navigation.state.routes[navigation.state.index];
-      return {
-        headerTitle: routeName
-      };
-    }
-  }
-);
-const DashboardStackNavigator = createStackNavigator(
-  {
-    DashboardTabNavigator: DashboardTabNavigator
-  },
-  {
-    defaultNavigationOptions: ({ navigation }) => {
-      return {
-        headerLeft: (
-          <Icon
-            style={{ paddingLeft: 10 }}
-            onPress={() => navigation.openDrawer()}
-            name="menu"
-            size={30}
-          />
-        )
-      };
-    }
-  }
-);
+// const DashboardTabNavigator = createBottomTabNavigator(
+//   {
+//     Feed,
+//     Profile,
+//     Settings
+//   },
+//   {
+//     navigationOptions: ({ navigation }) => {
+//       const { routeName } = navigation.state.routes[navigation.state.index];
+//       return {
+//         headerTitle: routeName
+//       };
+//     }
+//   }
+// );
+// const DashboardStackNavigator = createStackNavigator(
+//   {
+//     DashboardTabNavigator: DashboardTabNavigator
+//   },
+//   {
+//     defaultNavigationOptions: ({ navigation }) => {
+//       return {
+//         headerLeft: (
+//           <Icon
+//             style={{ paddingLeft: 10 }}
+//             onPress={() => navigation.openDrawer()}
+//             name="menu"
+//             size={30}
+//           />
+//         )
+//       };
+//     }
+//   }
+// );
 
 const AppDrawerNavigator = createDrawerNavigator({
   Dashboard: {
-    screen: DashboardStackNavigator
+    screen: Feed
+  },
+  Settings: {
+    screen: Settings
+  },
+  Profile: {
+    screen: Profile
   }
 });
 
 const AppSwitchNavigator = createSwitchNavigator({
   Welcome: { screen: WelcomeScreen },
+  CadastroPage: { screen: CadastroPage },
   Dashboard: { screen: AppDrawerNavigator }
 });
 
